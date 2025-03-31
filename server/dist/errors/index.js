f39d1c19c8b827c128bd8893b0a63896b6853b61
Progress Aienobe <paienobe@gmail.com>
2025-03-31 21:20:14 +0100

    removed dist folder

--- server/dist/errors/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NotFoundError = exports.CustomAPIError = exports.BadRequest = void 0;
-const bad_request_1 = require("./bad-request");
-Object.defineProperty(exports, "BadRequest", { enumerable: true, get: function () { return bad_request_1.BadRequest; } });
-const custom_api_1 = require("./custom-api");
-Object.defineProperty(exports, "CustomAPIError", { enumerable: true, get: function () { return custom_api_1.CustomAPIError; } });
-const not_found_1 = require("./not-found");
-Object.defineProperty(exports, "NotFoundError", { enumerable: true, get: function () { return not_found_1.NotFoundError; } });
